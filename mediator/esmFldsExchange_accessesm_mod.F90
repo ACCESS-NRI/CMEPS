0a044692afce74a218306b0c88afccc06c9161f4
Kieran Ricardo <u5824685@anu.edu.au>
2026-05-04 15:24:29 +1000

    Update mediator/esmFldsExchange_accessesm_mod.F90
    Co-authored-by: Spencer Wong <88933912+blimlim@users.noreply.github.com>

--- mediator/esmFldsExchange_accessesm_mod.F90
+++ mediator/esmFldsExchange_accessesm_mod.F90
@@ -503,7 +503,7 @@ module esmFldsExchange_accessesm_mod
       S_flds = (/'So_dhdx', & ! sea_surface_slope_zonal
                  'So_dhdy', & ! sea_surface_slope_merid
                  'So_t ', & ! inst_temp_height2m
-                 'So_s ', & ! inst_spec_humid_height2m
+                 'So_s ', & ! sea surface salinity
                  'So_u', & ! ocean surface zonal current
                  'So_v', & ! ocean surface meridional current
                  'Fioo_q' /) ! inst_temp_height_surface
