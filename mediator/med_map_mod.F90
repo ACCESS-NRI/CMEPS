17ddb2ee5410615570445bce1e1e2bee524f6f91
Kieran Ricardo <kieran.ricardo@anu.edu.au>
2026-07-15 13:35:00 +1000

    Wind stress mappatch_uv3d (#25)
    ### Description of changes
    
    Use `mappatch_uv3d` to map the windstress from the atmosphere lat-long
    grid to the ocn/ice tripolar grid. Interpolating the zonal/meridional
    components directly causes the components to almost cancel at the north
    pole, leading to a perpetual near
    `0` windstress at the north pole. Interpolating the 3D velocities
    instead fixes this problem.
    
    Addresses https://github.com/ACCESS-NRI/dev_coupling/issues/74

--- mediator/med_map_mod.F90
+++ mediator/med_map_mod.F90
@@ -950,6 +950,7 @@ contains
     use med_internalstate_mod , only : nmappers, mapfcopy
     use med_internalstate_mod , only : mappatch_uv3d, mappatch, mapbilnr_uv3d, mapconsf_uv3d, mapbilnr
     use med_internalstate_mod , only : packed_data_type
+    use med_internalstate_mod , only : coupling_mode
     use med_methods_mod       , only : Field_diagnose => med_methods_Field_diagnose
 
     ! input/output variables
@@ -1031,7 +1032,12 @@ contains
           if (mapindex == mappatch_uv3d) then
 
              ! For mappatch_uv3d do not use packed field bundles
-             call med_map_uv_cart3d(FBsrc, FBdst, routehandles, mappatch_uv3d, rc=rc)
+             if (trim(coupling_mode) == 'access-esm') then
+                call med_map_uv_cart3d(FBsrc, FBdst, routehandles, mappatch_uv3d, map_stress=.true., rc=rc)
+             else
+                call med_map_uv_cart3d(FBsrc, FBdst, routehandles, mappatch_uv3d, rc=rc)
+             end if
+
              if (chkerr(rc,__LINE__,u_FILE_u)) return
 
           else if (mapindex == mapbilnr_uv3d) then
